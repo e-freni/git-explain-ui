af9b421eafc669b024b81aa9d8ef58199064b43f
Enrico Freni <enrico.freni@jaewa.com>
2025-03-19 17:57:03 +0100

    added dedicated service for commands feedbacks

--- src/app/services/command-feedback.service.ts
+++ src/app/services/command-feedback.service.ts
@@ -1,15 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Type } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommandFeedbackService {
 
-  //TODO add feedback system instead of using the terminal component to handle it
+  private currentFeedbackSubject = new BehaviorSubject<string | null>(null);
+  private terminalOutputSubject = new BehaviorSubject<string[]>([]);
+  terminalOutput$ = this.terminalOutputSubject.asObservable();
 
   constructor() { }
 
-  getFeedback(command: string): string | null {
-    return null
+  setFeedbackByComponent(component: Type<any> | null, command: string): void {
+    if(!component){
+      this.setFeedback(`Command not found: ${command}`);
+      return;
+    }
+    this.setFeedback(command);
+  }
+
+  setFeedback(feedback: string): void {
+    this.currentFeedbackSubject.next(feedback);
+    this.updateOutput(feedback);
+  }
+
+  updateOutput(feedback: string){
+    let output = this.terminalOutputSubject.getValue();
+    output.push(feedback);
+    this.terminalOutputSubject.next(output);
   }
 }
