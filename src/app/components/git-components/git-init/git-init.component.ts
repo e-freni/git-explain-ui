e8e1f6203c500453d7f34fcd951e94aadb21dce9
Enrico Freni <enrico@freni.it>
2025-03-30 23:14:22 +0200

    added chartjs for tree drawing and simple example

--- src/app/components/git-components/git-init/git-init.component.ts
+++ src/app/components/git-components/git-init/git-init.component.ts
@@ -1,26 +1,37 @@
-import {NgIf, NgOptimizedImage} from '@angular/common';
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgIf} from '@angular/common';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Subject, takeUntil} from 'rxjs';
 import {CommandContentCheckerService} from '../../../services/command-content-checker.service';
 import {CommandFeedbackService} from '../../../services/command-feedback.service';
 import {CommandService} from '../../../services/command.service';
 import {TranslatePipe} from '@ngx-translate/core';
+import {
+  Chart,
+  ScatterController,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+Chart.register(ScatterController, LinearScale, PointElement, LineElement, Tooltip, Legend);
 
 @Component({
   selector: 'app-git-init',
   imports: [
-    NgOptimizedImage,
     NgIf,
     TranslatePipe
   ],
   templateUrl: './git-init.component.html',
   styleUrl: './git-init.component.css'
 })
-export class GitInitComponent implements OnInit, OnDestroy {
+export class GitInitComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('gitGraphContainer', { static: true }) containerRef!: ElementRef<HTMLDivElement>;
 
   public command: string | null = '';
   private destroy$ = new Subject<void>();
-  private projectHome = "/home/user/opt/your-super-software-project/" //TODO this variable should become dynamic in order to simulate OS behaviour
+  private projectHome = "/home/user/code/your-super-software-project/" //TODO this variable should become dynamic in order to simulate OS behaviour
 
   constructor(
     private commandService: CommandService,
@@ -36,6 +47,94 @@ export class GitInitComponent implements OnInit, OnDestroy {
         this.command = command;
         this.updateCommandFeedback()
       });
+  }
+
+
+  ngAfterViewInit(): void {
+    const ctx = document.getElementById('gitChart') as HTMLCanvasElement;
+
+    //TODO find a way to don't write all this boilercode
+    new Chart(ctx, {
+      type: 'scatter',
+      data: {
+        datasets: [
+          {
+            label: 'main',
+            data: [
+              { x: 0, y: 0, label: 'Init' },
+              { x: 0, y: 4, label: 'merge on main' },
+            ],
+            showLine: true,
+            borderColor: 'black',
+            backgroundColor: '#0078ef',
+            pointRadius: 10,
+            borderWidth: 2,
+            order: 1
+          },
+          {
+            label: 'develop',
+            data: [
+              { x: 0, y: 0, label: 'Init' },
+              { x: 1, y: 1, label: 'Start Feature' },
+              { x: 1, y: 2, label: 'feature 1' },
+              { x: 1, y: 3, label: 'polish' },
+              { x: 0, y: 4, label: 'merge on main' },
+            ],
+            showLine: true,
+            borderColor: 'black',
+            backgroundColor: '#6126e8',
+            pointRadius: 10,
+            borderWidth: 2,
+            order: 2
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: function (context: any) {
+                return context.raw.label;
+              },
+            },
+          },
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            min: -2,
+            max: 3,
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+              stepSize: 1,
+              callback: (val) => {
+                if (val === 0) return 'main';
+                if (val === 1) return 'develop';
+                return '';
+              },
+            },
+            grid: {
+              display: false,
+            },
+          },
+          y: {
+            type: 'linear',
+            ticks: {
+              display: false,
+            },
+            grid: {
+              display: false,
+            },
+            border: {
+              display: false,
+            },
+          },
+        },
+      },
+    });
   }
 
   ngOnDestroy(): void {
